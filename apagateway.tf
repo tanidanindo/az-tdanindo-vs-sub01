403f8f3ee49dc26bcda8dd567c2365d7ebc8d170
devopstani <devopstani@gmail.com>
2026-01-25 00:10:57 -0800

    ef

--- apagateway.tf
+++ apagateway.tf
@@ -4,16 +4,16 @@
 # #   name = var.resource_group_name
 # }
 
-# module "public_ip" {
-#   source              = "https://dev.azure.com/brucepowerit/Azure%20IAC%20-%20Workloads/_git/module-terraform-azurerm-public-ip?path=/example/main.tf"
-#   name                = var.app_gateway_public_ip_name
-#   resource_group_name = var.resource_group_name
-#   location            = var.location
-#   allocation_method   = "Dynamic"
-#   sku                 = "Standard"
-#   ip_version          = "IPv4"
+module "public_ip" {
+  source              = "Azure/avm-res-network-publicipaddress/azurerm"
+  name                = var.app_gateway_public_ip_name
+  resource_group_name = var.app_gateway_rg_name
+  location            = var.location
+  allocation_method   = "Dynamic"
+  sku                 = "Standard"
+  ip_version          = "IPv4"
 
-# }
+}
 
 
 
@@ -21,8 +21,7 @@ module "application_gateway" {
   source = "Azure/avm-res-network-applicationgateway/azurerm"
 
 
-  create_public_ip = true
-  public_ip_name   = var.app_gateway_public_ip_name
+  create_public_ip = false
   frontend_ip_configuration_private = {
     name                          = "Private"
     private_ip_address_allocation = "Dynamic"
