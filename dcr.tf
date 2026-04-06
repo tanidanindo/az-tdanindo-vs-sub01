0c9ff78472f5d276a9af3e0ddc5216cf8f064e08
devopstani <devopstani@gmail.com>
2026-04-05 22:08:23 -0700

    u

---
+++ dcr.tf
@@ -0,0 +1,53 @@
+resource "azurerm_monitor_data_collection_rule" "example" {
+  name                = "dcr-example"
+  resource_group_name = "rg-sentinel-migration"
+  location            = var.resource_group_location
+
+
+  data_sources {
+    syslog {
+      facility_names = ["*"]
+      log_levels     = ["*"]
+      name           = "logstash-syslog"
+      streams        = ["Microsoft-Syslog"]
+    }
+  }
+  destinations {
+    log_analytics {
+      name                  = "tanilaw01"
+      workspace_resource_id = "/subscriptions/f1e531f4-e1b0-486c-bb3c-a2e0a49d0121/resourceGroups/rg-td-vs01/providers/Microsoft.OperationalInsights/workspaces/tanilaw01"
+    }
+  }
+  data_flow {
+    streams       = ["Example"]
+    destinations  = ["tanilaw01"]
+    output_stream = "Example_CL"
+  }
+  stream_declaration {
+    stream_name = "logstash-syslog-stream"
+    column {
+      name = "TimeGenerated"
+      type = "datetime"
+    }
+    column {
+      name = "Level"
+      type = "string"
+    }
+    column {
+      name = "Logger"
+      type = "string"
+    }
+    column {
+      name = "Context"
+      type = "string"
+    }
+    column {
+      name = "Message"
+      type = "string"
+    }
+    column {
+      name = "AdditionalContext"
+      type = "string"
+    }
+  }
+}
