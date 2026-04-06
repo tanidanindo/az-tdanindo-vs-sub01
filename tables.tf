8045e423a2eb6ea3c6bd8714e4cbab86e4c1ce70
devopstani <devopstani@gmail.com>
2026-04-05 20:59:14 -0700

    o

--- tables.tf
+++ tables.tf
@@ -48,7 +48,7 @@ resource "azapi_resource" "data_collection_logs_table" {
 }
 
 
-resource "azapi_resource" "data_collection_logs_table" {
+resource "azapi_resource" "data_collection_logs_table2" {
   name      = "Example_CL2"
   parent_id = "/subscriptions/f1e531f4-e1b0-486c-bb3c-a2e0a49d0121/resourceGroups/rg-td-vs01/providers/Microsoft.OperationalInsights/workspaces/tanilaw01"
   type      = "Microsoft.OperationalInsights/workspaces/tables@2022-10-01"
@@ -59,29 +59,34 @@ resource "azapi_resource" "data_collection_logs_table" {
           "name" : "Example_CL",
           "columns" : [
             {
-              "description" : "The time at which the data was generated",
               "name" : "TimeGenerated",
-              "type" : "datetime"
+              "type" : "datetime",
+              "description" : "The time at which the data was generated"
             },
             {
-              "description" : "The computer that generated the data",
-              "name" : "Computer",
-              "type" : "string"
+              "name" : "Level",
+              "type" : "string",
+              "description" : "Log level"
             },
             {
-              "description" : "Additional message properties",
+              "name" : "Logger",
+              "type" : "string",
+              "description" : "Logger name"
+            },
+            {
+              "name" : "Context",
+              "type" : "string",
+              "description" : "Context of the log line"
+            },
+            {
+              "name" : "Message",
+              "type" : "string",
+              "description" : "Log message"
+            },
+            {
               "name" : "AdditionalContext",
-              "type" : "dynamic"
-            },
-            {
-              "description" : "Name of the counter",
-              "name" : "CounterName",
-              "type" : "string"
-            },
-            {
-              "description" : "Value collected for the counter",
-              "name" : "CounterValue",
-              "type" : "real"
+              "type" : "string",
+              "description" : "Additional context of the log line"
             }
           ]
         },
