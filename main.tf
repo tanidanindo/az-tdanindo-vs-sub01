0aa778a1d066a0cd252ba752b719b61b816fd6f7
devopstani <devopstani@gmail.com>
2026-04-05 20:08:56 -0700

    new stuff 3

--- main.tf
+++ main.tf
@@ -8,16 +8,16 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-# resource "azurerm_log_analytics_workspace" "tanilaw01" {
-#   # Configuration details
-#   name                = "tanilaw01" # Must be globally unique
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   sku                 = "PerGB2018" # Pricing Tier (e.g., Free, PerGB2018, Consumption, etc.)
+resource "azurerm_log_analytics_workspace" "tanilaw01" {
+  # Configuration details
+  name                = "tanilaw01" # Must be globally unique
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018" # Pricing Tier (e.g., Free, PerGB2018, Consumption, etc.)
 
-#   # Retention setting (data older than this is purged)
-#   retention_in_days = 30
-# }
+  # Retention setting (data older than this is purged)
+  retention_in_days = 30
+}
 
 
 resource "azurerm_resource_group" "rg-sentinel-migration" {
