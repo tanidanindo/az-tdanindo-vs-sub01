ea83fa6b810cb79730e782cce617e3b1f765f892
devopstani <devopstani@gmail.com>
2026-04-07 20:01:30 -0700

    new table

--- tables.tf
+++ tables.tf
@@ -137,3 +137,60 @@ resource "azapi_resource" "data_collection_logs_table3" {
     }
   )
 }
+
+
+resource "azapi_resource" "data_collection_logs_table3" {
+  name      = "Example3_CL"
+  parent_id = "/subscriptions/f1e531f4-e1b0-486c-bb3c-a2e0a49d0121/resourceGroups/rg-td-vs01/providers/Microsoft.OperationalInsights/workspaces/tanilaw01"
+  type      = "Microsoft.OperationalInsights/workspaces/tables@2022-10-01"
+  body = jsonencode(
+    {
+      "properties" : {
+        "schema" : {
+          "name" : "bp_algosec_aux_CL",
+          "columns" : [
+            {
+              "name" : "TimeGenerated",
+              "type" : "datetime"
+            },
+            {
+              "name" : "event",
+              "type" : "string"
+            },
+            {
+              "name" : "process",
+              "type" : "string"
+            },
+            {
+              "name" : "service",
+              "type" : "string"
+            },
+            {
+              "name" : "message",
+              "type" : "string"
+            },
+            {
+              "name" : "log",
+              "type" : "string"
+            },
+            {
+              "name" : "host",
+              "type" : "string"
+            },
+            {
+              "name" : "ls_timestamp",
+              "type" : "datetime"
+            },
+            {
+              "name" : "ls_version",
+              "type" : "string"
+            }
+
+          ]
+        },
+        "retentionInDays" : 30,
+        "totalRetentionInDays" : 30
+      }
+    }
+  )
+}
