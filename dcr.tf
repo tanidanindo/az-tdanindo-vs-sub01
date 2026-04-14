aca95f9b217529bdae8fe84440b7534f7d15d9a0
devopstani <devopstani@gmail.com>
2026-04-14 16:41:15 -0700

    i

--- dcr.tf
+++ dcr.tf
@@ -52,7 +52,6 @@ resource "azapi_resource" "data_collection_rule_1" {
   name      = "dcr01"
   parent_id = "/subscriptions/f1e531f4-e1b0-486c-bb3c-a2e0a49d0121/resourceGroups/rg-sentinel-migration"
   type      = "microsoft.insights/datacollectionrules@2022-06-01"
-  location  = var.resource_group_location
   body = jsonencode(
     {
       kind     = "Direct"
