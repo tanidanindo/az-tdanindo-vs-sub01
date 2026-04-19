b3f92d055d88b6ce1074a901052d79d19c93fc4c
devopstani <devopstani@gmail.com>
2026-04-19 16:49:58 -0700

    g

--- dcr.tf
+++ dcr.tf
@@ -48,64 +48,64 @@ resource "azurerm_monitor_data_collection_rule" "example" {
 
 
 
-# resource "azapi_resource" "data_collection_rule_1" {
-#   name      = "dcr01"
-#   parent_id = "/subscriptions/f1e531f4-e1b0-486c-bb3c-a2e0a49d0121/resourceGroups/rg-sentinel-migration"
-#   type      = "microsoft.insights/datacollectionrules@2022-06-01"
-#   body = jsonencode(
-#     {
-#       kind     = "Direct"
-#       location = var.resource_group_location
-#       properties = {
-#         streamDeclarations = {
-#           Custom-Example2_CL = {
-#             columns = [
-#               {
-#                 name = "TimeGenerated"
-#                 type = "datetime"
-#               },
-#               {
-#                 name = "Level"
-#                 type = "string"
-#               },
-#               {
-#                 name = "Logger"
-#                 type = "string"
-#               },
-#               {
-#                 name = "Context"
-#                 type = "string"
-#               },
-#               {
-#                 name = "Message"
-#                 type = "string"
-#               },
-#               {
-#                 name = "AdditionalContext"
-#                 type = "string"
-#               }
-#             ]
-#           }
-#         }
+resource "azapi_resource" "data_collection_rule_1" {
+  name      = "dcr01"
+  parent_id = "/subscriptions/f1e531f4-e1b0-486c-bb3c-a2e0a49d0121/resourceGroups/rg-sentinel-migration"
+  type      = "microsoft.insights/datacollectionrules@2022-06-01"
+  body = jsonencode(
+    {
+      kind     = "Direct"
+      location = var.resource_group_location
+      properties = {
+        streamDeclarations = {
+          "Custom-Example2_CL" = {
+            columns = [
+              {
+                name = "TimeGenerated"
+                type = "datetime"
+              },
+              {
+                name = "Level"
+                type = "string"
+              },
+              {
+                name = "Logger"
+                type = "string"
+              },
+              {
+                name = "Context"
+                type = "string"
+              },
+              {
+                name = "Message"
+                type = "string"
+              },
+              {
+                name = "AdditionalContext"
+                type = "string"
+              }
+            ]
+          }
+        }
 
-#         destinations = {
-#           logAnalytics = [
-#             {
-#               workspaceResourceId = "/subscriptions/f1e531f4-e1b0-486c-bb3c-a2e0a49d0121/resourceGroups/rg-td-vs01/providers/Microsoft.OperationalInsights/workspaces/tanilaw01"
-#               name                = "tanilaw01"
-#             }
-#           ]
-#         }
+        destinations = {
+          logAnalytics = [
+            {
+              workspaceResourceId = "/subscriptions/f1e531f4-e1b0-486c-bb3c-a2e0a49d0121/resourceGroups/rg-td-vs01/providers/Microsoft.OperationalInsights/workspaces/tanilaw01"
+              name                = "tanilaw01"
+            }
+          ]
+        }
 
-#         dataFlows = [
-#           {
-#             streams      = ["Custom-Example2_CL"]
-#             destinations = ["tanilaw01"]
-#             transformKql = "source|extend TimeGenerated = todatetime(ls_timestamp)\n"
-#             outputStream = "Custom-Example2_CL"
-#           }
-#         ]
-#       }
-#     }
-#   )
-# }
+        dataFlows = [
+          {
+            streams      = ["Custom-Example2_CL"]
+            destinations = ["tanilaw01"]
+            transformKql = "source|extend TimeGenerated = todatetime(ls_timestamp)\n"
+            outputStream = "Custom-Example2_CL"
+          }
+        ]
+      }
+    }
+  )
+}
